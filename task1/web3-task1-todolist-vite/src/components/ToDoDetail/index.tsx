3e5f42d71399b87262f27675098fe2e50eab7cbd
小莫唐尼 <1431128779@qq.com>
2024-06-15 11:40:40 +0800

    update: 调整详情样式

--- task1/web3-task1-todolist-vite/src/components/ToDoDetail/index.tsx
+++ task1/web3-task1-todolist-vite/src/components/ToDoDetail/index.tsx
@@ -26,32 +26,32 @@ const ToDoDetail = ({item}: IProps) => {
 
     return (
         <>
-            <div className="flex-col items-center mt-16 p-3 w-full border border-gray-200 rounded-md text-gray-600">
+            <div className="flex-col items-center mt-16 p-3 w-full border border-gray-200 rounded-md text-gray-800">
                 <div className="border-b border-gray-200 mb-4">
-                    <h1 className="text-gray-950 font-semibold text-md mb-1"> 待办详情 </h1>
+                    <h1 className="text-gray-950 font-semibold  mb-1"> 待办详情 </h1>
                 </div>
                 {
                     todo && (
-                        <div className="flex flex-col gap-y-4 text-md">
-                            <h3><span className="text-md">待办标题</span>：<span className="font-bold">{todo.title}</span></h3>
+                        <div className="flex flex-col gap-y-3 text-sm">
+                            <h3><span className="">待办标题</span>：<span className="font-bold">{todo.title}</span></h3>
                             <p className="mt-3">
-                                <span className="text-md">待办内容</span>：{todo.description}
+                                <span className="">待办内容</span>：{todo.description}
+                            </p>
+                            <p>
+                                <span className="">待办状态</span>：{todoStateMap[todo.state]}
                             </p>
                             {
                                 item?.createdAt && (
                                     <p>
-                                        <span className="text-md">创建时间</span>：{format(new Date(item?.createdAt), "yyyy年mm月dd日 HH点mm分ss秒")}
+                                        <span className="">创建时间</span>：{format(new Date(item?.createdAt), "yyyy年mm月dd日 HH点mm分ss秒")}
                                     </p>
                                 )
                             }
-                            <p>
-                                <span className="text-md">待办状态</span>：{todoStateMap[todo.state]}
-                            </p>
                             {
                                 (todo.state === 'complete' && todo.completeAt) &&
                                 (
                                     <p>
-                                        <span className="text-md">完成时间</span>：{format(new Date(todo.completeAt), "yyyy年mm月dd日 HH点mm分ss秒")}
+                                        <span className="">完成时间</span>：{format(new Date(todo.completeAt), "yyyy年mm月dd日 HH点mm分ss秒")}
                                     </p>
                                 )
                             }
